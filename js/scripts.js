21a8d139c53f1731743ca36d559ef32ec3180ce4
prudence naliaka <prudencenaliaka@gmail.com>
2019-03-05 07:39:10 +0300

    added an else if loop to display the winner

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,19 @@ $(document).ready(function(){
     var firstScore = Math.floor(Math.random() *6 )+1;
     firstdie.innerHTML=firstScore;
     moringa.innerHTML= ("You rolled " + firstScore +". ");
-    var hold=document.getElementById('hold');
+    var hold1=document.getElementById('hold1');
     var cScore=document.getElementById('cScore');
-    var cummulativeScore =initialScore+=firstScore;
-    cScore.innerHTML=("your cummulative score is " + cummulativeScore);
+    var cumulativeScore =initialScore+=firstScore;
+    cScore.innerHTML=("your cumulative score is " + cumulativeScore);
     if (firstScore===1){
-      hold.innerHTML=("it is the next players turn.");
-    }else {
-      hold.innerHTML=("continue");
+      hold1.innerHTML=("it is the next players turn.");
+    }else if(cumulativeScore===100){
+       hold1.innerHTML=("Bravo!!!firstplayer won the game.");
     }
+    else {
+      hold1.innerHTML=("continue");
+    }
+
     });
   $("#roll-seconddie").click(function(){
     var seconddie=document.getElementById('seconddie')
@@ -22,14 +26,17 @@ $(document).ready(function(){
     var secondScore = Math.floor(Math.random() *6 )+1;
     seconddie.innerHTML=secondScore;
     moringah.innerHTML= ("You rolled " + secondScore +". ");
-    var stop=document.getElementById('stop');
+    var hold2=document.getElementById('hold2');
     var cScoree=document.getElementById('cScoree');
-    var cummulativeScore =initialScore+=secondScore;
-    cScoree.innerHTML=("your cummulative score is " + cummulativeScore);
+    var cumulativeScore =initialScore+=secondScore;
+    cScoree.innerHTML=("your cumulative score is " + cumulativeScore);
     if (secondScore===1){
-      stop.innerHTML=("it is the next players turn.");
-    }else {
-      stop.innerHTML=("continue");
+      hold2.innerHTML=("it is the next players turn.");
+    }else if(cumulativeScore===100){
+        hold2.innerHTML=("Bravo!!!secondplayer won the game.");
+    }
+    else {
+      hold2.innerHTML=("continue");
     }
  });
 });
